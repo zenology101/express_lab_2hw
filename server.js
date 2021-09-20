684d19a59541eeef1405492aed6fc85862678d98
Zen Ahmed <zenahmed994@gmail.com>
2021-09-19 22:41:48 -0400

    Magic 8 Ball

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port =  3000;
+const eightBall = require("./model/8ball")
 
 
 app.get('/greeting/:name', (req, res) => {
@@ -14,6 +15,15 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send("tip:" + total * tipPercentage/100);
 })
 
+app.get("/magic/:phrase", (req,res) => {
+    //copied the random function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    function index (max){
+        return Math.floor(Math.random() * max)
+    };
+    let randomBall = index(eightBall.length)
+
+    res.send(`<h1> ${req.params.phrase + ": " + eightBall[randomBall]}</h1>`)
+})
 
 
 app.listen(port,() => {
